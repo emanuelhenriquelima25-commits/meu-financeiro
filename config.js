d6e094d43fb245817879451d5dc0c8bb09895f2b
emanuelhenriquelima25-commits <emanuelhenriquelima25@gmail.com>
2026-08-11 23:49:55 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Configuração do Supabase
-// Esta chave é pública (publishable/anon) e pode ficar no frontend.
-window.SUPABASE_URL = "https://sndpfgxqwsvacsimpszk.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_xFvJISqAURUFO7u0ns93Kg_mg-bOv7g";
