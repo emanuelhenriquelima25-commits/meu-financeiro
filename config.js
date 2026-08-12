293246f887591ba0692258a3ac922f3cb9a3ce5d
emanuelhenriquelima25-commits <emanuelhenriquelima25@gmail.com>
2026-08-11 23:59:21 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configuração do Supabase
+// Esta chave é pública (publishable/anon) e pode ficar no frontend.
+window.SUPABASE_URL = "https://sndpfgxqwsvacsimpszk.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_xFvJISqAURUFO7u0ns93Kg_mg-bOv7g";
